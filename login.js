0e2b86e894728f328edfdbc3643fa044daead1a6
Sreevalli Eswari Veerni <sreevallieswari.veerni@cytechies.in>
2026-07-30 10:02:24 -0700

    first commit

--- login.js
+++ login.js
@@ -60,3 +60,52 @@ function showRegisterPassword(){
     }
 
 }
+// ===============================
+// DARK MODE
+// ===============================
+
+const themeBtn = document.getElementById("theme-btn");
+
+if (themeBtn) {
+
+    if (localStorage.getItem("theme") === "dark") {
+        document.body.classList.add("dark");
+        themeBtn.innerHTML = '<i class="fa-solid fa-sun"></i>';
+    }
+
+    themeBtn.addEventListener("click", function () {
+
+        document.body.classList.toggle("dark");
+
+        if (document.body.classList.contains("dark")) {
+            localStorage.setItem("theme", "dark");
+            themeBtn.innerHTML = '<i class="fa-solid fa-sun"></i>';
+        } else {
+            localStorage.setItem("theme", "light");
+            themeBtn.innerHTML = '<i class="fa-solid fa-moon"></i>';
+        }
+
+    });
+
+}
+
+
+// ===============================
+// RTL MODE
+// ===============================
+
+const rtlBtn = document.getElementById("rtl-btn");
+
+if (rtlBtn) {
+
+    rtlBtn.addEventListener("click", function () {
+
+        if (document.documentElement.dir === "rtl") {
+            document.documentElement.dir = "ltr";
+        } else {
+            document.documentElement.dir = "rtl";
+        }
+
+    });
+
+}
